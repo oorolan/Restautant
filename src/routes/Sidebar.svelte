58804744473a0c133d87cdb803a483821fcac6fa
Rlimascca <128554741+oorolan@users.noreply.github.com>
2026-09-13 16:21:05 -0500

    feat: nuevo logo de RestaurantStock y version 1.0.1

--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -56,9 +56,7 @@
 <aside class="sidebar">
   <div class="sidebar-logo">
     <div class="sidebar-logo-icon">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="22" height="22">
-        <path stroke-linecap="round" stroke-linejoin="round" d={icons.logo} />
-      </svg>
+      <img src="/logo.png" alt="Logo" class="app-logo-img" />
     </div>
     <div class="sidebar-logo-text">
       <span class="app-name">{$configuracion.nombre_comercial || 'RestaurantStock'}</span>
